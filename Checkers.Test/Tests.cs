ae8dd0af709cf165c4565a2733094ad7ddc0b296
YoungMiGo123 <martinkleinbooi@gmail.com>
2018-04-12 16:27:52 +0200

    Add test: A board must have 12 white and 12 black pieces

---
+++ Checkers.Test/Tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Checkers.Test
+{
+    [TestFixture]
+    class Tests
+    {
+       [Test]
+       public void ABoardHas12BlackAnd12WhitePieces()
+        {
+            Board b = new Board();
+            int blackCount = b.Pieces(Color.Black).Count();
+            int WhiteCount = b.Pieces(Color.White).Count();
+            Assert.That(blackCount == 12);
+            Assert.That(WhiteCount == 12);
+        }
+    }
+}
